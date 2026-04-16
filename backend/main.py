edea04625a0222874b88744956f407ce0fa0e0de
kantsverma <kantsverma@users.noreply.github.com>
2026-04-17 02:24:39 +0530

    Change pythong host for render use and comment it for localhost again

--- backend/main.py
+++ backend/main.py
@@ -55,4 +55,4 @@ if __name__ == "__main__":
     # uvicorn.run(app, host="127.0.0.1", port=8000)
 
     # Render / production
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=8000)    
